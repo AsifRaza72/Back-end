a136aa6d52918406f86694c5110cfd5347d19944
AsifRaza72 <asifaligohari572@gmail.com>
2023-10-09 16:47:27 +0500

    9-10-23

---
+++ 09-10-2023/lists.py
@@ -0,0 +1,8 @@
+fruits=["Pineapple","Banana","Apple","Melon"]
+#insert element in last
+# fruits.append("kiwi")
+#insert element in desired index
+fruits.insert(0,"orange")
+# fruits.remove("Apple")
+print(fruits.pop(3))
+print(fruits)
