a6c9325272f177d6f542f01b9369493fc0088b9e
AsifRaza72 <asifaligohari572@gmail.com>
2023-10-23 16:42:54 +0500

    done

---
+++ 23-10-2023/mapFunction.py
@@ -0,0 +1,20 @@
+# menu=["espresso","mocha","latte","cappuccino","cortado","americano"]
+
+# def find_coffee(coffee):
+#     if coffee[0]=='c':
+#         return coffee
+# map_coffee=map(find_coffee,menu)
+# print(map_coffee)
+# for x in map_coffee:
+#     print(x)   
+
+
+
+menu=[1,2,3,4,5,6,7,8,9]
+
+def find_num(num):
+  return num*2
+map_num=map(find_num,menu)
+print(map_num)
+for x in map_num:
+    print(x)    
